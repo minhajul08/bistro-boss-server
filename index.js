e75f3b1dacf834879b33b58199ef422a0be679e5
minhajul08 <minhazmehadi61@gmail.com>
2024-09-10 00:29:29 +0600

    Added route handler for PATCH /users/admin/:id to update user role to 'admin'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express ();
+const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -9,8 +9,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware 
 
 
-app.use (cors());
-app.use (express.json ());
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -29,39 +29,78 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("review");
     const cartCollection = client.db("bistroDb").collection("carts");
 
-    app.get ('/menu', async (req,res) => {
-        const result = await menuCollection.find().toArray();
-        res.send (result);
+    // user related api
+
+    app.get('/users', async (req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send (result);
     })
-    app.get ('/reviews', async (req,res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send (result);
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send( {message:'The user is already existing', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch ('/users/admin/:id', async (req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId (id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updateDoc)
+      res.send (result)
+    })
+
+    app.delete ('/users/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId (id)}
+      const result = await userCollection.deleteOne(query)
+      res.send (result);
+    })
+
+    // menu related api
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
 
     // carts collection 
 
-    app.get ('/carts', async (req,res) => {
-       const email = req.query.email;
-       const query = {email: email}
-       const result = await cartCollection.find(query).toArray();
-       res.send (result);
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
     })
 
-    app.post ('/carts', async (req,res) => {
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
-      const result = await cartCollection.insertOne (cartItem);
-      res.send (result);
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
-    app.delete ('/carts/:id', async (req,res) => {
-      const id =req.params.id;
-      const query = {_id : new ObjectId (id)}
-      const result = await cartCollection.deleteOne (query)
-      res.send (result);
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
     })
 
 
@@ -75,10 +114,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get ('/', (req,res) => {
-    res.send ('boss is sitting')
+app.get('/', (req, res) => {
+  res.send('boss is sitting')
 })
 
-app.listen (port, () => {
-    console.log (`Bistro boss server is running ${port}`);
+app.listen(port, () => {
+  console.log(`Bistro boss server is running ${port}`);
 })
